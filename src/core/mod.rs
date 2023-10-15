9de830da76079284994e62cae6ad4ec1b735b98d
nicholasammann <ammannnicholas@gmail.com>
2023-10-15 14:48:25 -0700

    Changes

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,55 +1,14 @@
-use std::collections::HashMap;
 use std::string::String;
 
-#[derive(Debug)]
-pub enum PropertyValue {
-  Int(i32),
-  Float(f32),
-  Str(String)
+mod level;
+mod object;
+mod component;
+mod component_property;
+
+trait GetName {
+  fn get_name(&self) -> &str;
 }
 
-
-// Game
-//// Level
-
-
-// Level
-//// Objects
-#[derive(Debug)]
-pub struct Level {
-  pub name: String,
-  pub objects: HashMap<String, Object>
-}
-
-
-// Object
-//-- Name
-//-- Components
-//-- Traits for adding, removing components
-#[derive(Debug)]
-pub struct Object {
-  pub name: String,
-  pub components: HashMap<String, Component>
-}
-
-
-// Component
-//-- Name
-//-- Properties
-//-- Traits for adding, removing, setting, getting properties
-#[derive(Debug)]
-pub struct Component {
-  pub name: String,
-  pub properties: HashMap<String, ComponentProperty>
-}
-
-
-// Properties
-//-- Name
-//-- Value
-//-- Traits for setting/getting name and value
-#[derive(Debug)]
-pub struct ComponentProperty {
-  pub name: String,
-  pub value: PropertyValue
+trait SetName {
+  fn set_name(&mut self, new_name: String);
 }
